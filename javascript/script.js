107b96ea202d97865221b9fc6d319d664254017a
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-13 16:35:20 +0200

    Milestone 2

--- javascript/script.js
+++ javascript/script.js
@@ -114,18 +114,84 @@ mostriamo in pagina tutte le icone disponibili come da layout.
 */
 
 
-
-
 for(item of icons){
     //Destructuring each object in the array
     const {name, prefix, type, family} = item;
 
+    //Show the icons in the webpage
     document.getElementById('my_icons').innerHTML += `
     <div class="col p-4">
-        <i class="${family} ${prefix+name} fa-3x d-block"></i>
+        <i class="${family} ${prefix+name} fa-3x d-block" style="color: }"></i>
         <span>${name} </span>
     </div>`
-
 }
 
 
+/*
+Milestone 2: Coloriamo le icone per tipo
+*/
+
+//creare un array che mi da tutti i tipi una volta
+//creare un array che contiene  colori quanti sono i tipi
+const typesList = [];
+const colorsList = [];
+for(item of icons){
+    if(!typesList.includes(item.type)){
+        typesList.push(item.type)
+        colorsList.push(getRandomHEXColor())
+    }
+}
+
+
+// icons.forEach((item) =>{
+//     typesList.forEach((typesListitem, index) =>{
+//     if(item.type == typesListitem){
+//         item.color = colorsList[index];
+//     } else if(item.type == typesListitem) {
+//         item.color = colorsList[index];
+//     } else if(item.type == typesListitem) {
+//         item.color = colorsList[index];
+//     }});
+// });
+
+//Indipendentemente da quanti 'type' ci sono verrà assegnato loro un colore che dipende dal tipo  
+icons.forEach((item) =>{
+    let i = 0;
+    //Ciclo che dipende dal numero di type
+    while(i < typesList.length){
+        if(item.type == typesList[i]){
+            item.color = colorsList[i];
+        }
+        i++;
+    }
+});
+console.log(typesList);
+console.log(colorsList);
+
+console.log(icons);
+
+
+
+
+
+
+
+
+function pushNonRepeatedItemsInAnArray(receiverArray, itemToPush){
+
+    if (!receiverArray.includes(itemToPush)){
+  
+      receiverArray.push(itemToPush);
+    }
+  }
+
+
+
+
+
+/*
+Milestone 3: Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+*/
+
+// ++++++Functions++++++
+function getRandomHEXColor() { return '#' + Math.floor(Math.random()*16777215).toString(16); }
